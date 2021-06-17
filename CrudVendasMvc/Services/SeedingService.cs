5771f9070011ef296216f30f95a635d454337edf
Matheus Luis <66440299+causticsudo@users.noreply.github.com>
2021-06-17 02:37:58 -0300

    Update SeedingService.cs

--- CrudVendasMvc/Services/SeedingService.cs
+++ CrudVendasMvc/Services/SeedingService.cs
@@ -72,7 +72,11 @@ namespace CrudVendasMvc.Data
 
             _context.Vendedor.AddRange(s1, s2, s3, s4, s5, s6);
 
-
+                        _context.VendasRegistro.AddRange(
+                r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
+                r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
+                r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
+            );
 
             _context.SaveChanges();
         }
